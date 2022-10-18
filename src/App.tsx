dd2acaac13c0bddf707164239d321f51511c609e
lgnvmaksim <lgnvmaksim@gmail.com>
2022-10-18 16:51:01 +0300

    completed preparations for SP2

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,24 @@ import {Footer} from "./Microtasks/site/Footer";
 
 
 function App() {
+    // const [money, setMoney] = useState([
+    //     {banknote: 'dollar', nominal: 100, number: 'a1234567890'},
+    //     {banknote: 'dollar', nominal: 50, number: 'z1234567890'},
+    //     {banknote: 'ruble', nominal: 100, number: 'w1234567890'},
+    //     {banknote: 'dollar', nominal: 100, number: 'e1234567890'},
+    //     {banknote: 'dollar', nominal: 50, number: 'c1234567890'},
+    //     {banknote: 'ruble', nominal: 100, number: 'r1234567890'},
+    //     {banknote: 'dollar', nominal: 50, number: 'x1234567890'},
+    //     {banknote: 'ruble', nominal: 50, number: 'v1234567890'},
+    // ])
 
-    const [money, setMoney] = useState([
-        {banknote: 'dollar', nominal: 100, number: 'a1234567890'},
-        {banknote: 'dollar', nominal: 50, number: 'z1234567890'},
-        {banknote: 'ruble', nominal: 100, number: 'w1234567890'},
-        {banknote: 'dollar', nominal: 100, number: 'e1234567890'},
-        {banknote: 'dollar', nominal: 50, number: 'c1234567890'},
-        {banknote: 'ruble', nominal: 100, number: 'r1234567890'},
-        {banknote: 'dollar', nominal: 50, number: 'x1234567890'},
-        {banknote: 'ruble', nominal: 50, number: 'v1234567890'},
-    ])
     return (
         <div>
             <div>
                 <Header title={'New Header'}/>
                 <Body titleForBody={'New Body'}/>
                 <Footer titleForFooter={'New Footer'}/>
-                <FilteredMoneyWithButton coins={money}/>
+                {/*<FilteredMoneyWithButton coins={money}/>*/}
             </div>
         </div>
     );
