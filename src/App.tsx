47b7e70cc8bd6a16cd95bc69d72f4c36fc6414a3
lgnvmaksim <lgnvmaksim@gmail.com>
2022-10-24 15:59:53 +0300

    re-passing the task about input - DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import './App.css';
 import React, {useState} from "react";
+import {FullInput} from "./Microtasks/Input/Full-Input";
+import {Input} from "./Microtasks/Input/Input";
+import {Button} from "./Microtasks/Input/Button";
 
 
-export const App=()=> {
+function App() {
     const [message, setMessage] = useState([
             {message: 'message1'},
             {message: 'message2'},
             {message: 'message3'},
             {message: 'message4'},
             {message: 'message5'}
-        ]
-    )
+        ])
+    const messageButton = (t: string) => {
+        let newMessage = {message: t}
+        setMessage([newMessage,...message])   //сообщение выводит вписанное значение Input
+    }
+
+    let [title, setTitle] = useState('')
+
+    const callBackButtonHandler=()=>{
+        messageButton(title)
+        setTitle('')
+    }
+
     return (
         <div className="App">
             <div>
-                <input />
-                <button>+</button>
+                {/*<FullInput addMessage={messageButton}/>*/}
+                <Input setTitle={setTitle} title={title}/>
+                <Button name={'+'} callBack={callBackButtonHandler}/>
             </div>
             {message.map((el, index) => {
                 return (
@@ -25,3 +40,5 @@ export const App=()=> {
         </div>
     );
 }
+
+export default App
---
+++ src/Microtasks/Input/Input.tsx
@@ -0,0 +1,14 @@
+import {ChangeEvent} from "react";
+
+type InputPropsType={
+    setTitle:(title:string)=>void
+    title:string
+}
+
+export const Input = (props:InputPropsType)=>{
+    const onChangeInputHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        props.setTitle(e.currentTarget.value)}
+    return(
+        <input value={props.title} onChange={onChangeInputHandler}/>
+    )
+}
